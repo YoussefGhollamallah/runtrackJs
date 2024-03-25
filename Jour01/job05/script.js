77febaae6a7b42db13320244a295c49a4aa74b52
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-25 14:00:47 +0100

    Création d'une function afficherjourssemaine qui prend une liste et l'affiche via une boucle for

---
+++ Jour01/job05/script.js
@@ -0,0 +1,9 @@
+let joursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+function afficherjourssemaine() {
+    for (let i = 0; i < joursSemaine.length; i++) {
+        console.log(joursSemaine[i]);
+    }
+}
+
+console.log(afficherjourssemaine());
