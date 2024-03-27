55640b18116287b34cefafac3975d97d2245ac78
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-27 10:20:17 +0100

    Création d'un script en jQuery : ce script appel les boutons par leur id et affiche ou cache la citation.

---
+++ Jour03/job01/script.js
@@ -0,0 +1,9 @@
+$("#citation").text("Les logiciels et les cathédrales, c'est un peu la même chose. D'abord, on les construit, ensuite on prie.");
+
+$("#showbtn").click(function(){
+    $("#citation").show();
+});
+
+$("#hidebtn").click(function(){
+    $("#citation").hide();
+});
