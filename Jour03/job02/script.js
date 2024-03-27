1228b3b33688a9678548175d7ca9961c988130cf
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-27 15:30:57 +0100

    mise à jour d'une fonction : correction de la fonction checkOrder() ou maintenant je récupère le src de l'image et compare le numéro de l'index pour qu'il correspond à l'ordre croissant des images

--- Jour03/job02/script.js
+++ Jour03/job02/script.js
@@ -1,6 +1,8 @@
 $("#resultat").text("Vous avez cliqué sur le bouton !").css("color", "red").hide();
 // $("#resultat").hide();
 
+
+// fonctionnality to show the result when the button is clicked
 $.fn.shuffleChildren = function() {
     $.each(this.get(), function(index, el) {
       var $el = $(el);
@@ -15,12 +17,36 @@ $.fn.shuffleChildren = function() {
     });
   };
   
+
+// fonctionnality to shuffle the images in the second container
   $("#shuffleBtn").click(function() {
-    // Usage
     $("#secondContainer").shuffleChildren();
   });
 
+
+// fonctionnality to move an image from one container to another
   $("#secondContainer").on("click", "img", function() {
     var image = $(this).detach();
     $("#container").append(image);
+    if ($("#container img").length === 6) {
+        checkOrder();
+    }
 });
+
+
+function checkOrder() {
+    let images = $("#container");
+    // let image = images.children().attr("src");
+    let isOrdered = true;
+    images.children().each(function(index, img) {
+        if ($(img).attr("src") !== "images/arc" + (index + 1) + ".png") {
+            isOrdered = false;
+        }
+    });
+
+    if (isOrdered) {
+        $("#resultat").text("Vous avez gagné !").css("color", "green").show();
+    } else {
+        $("#resultat").text("Vous avez perdu !").css("color", "red").show();
+    }
+}
