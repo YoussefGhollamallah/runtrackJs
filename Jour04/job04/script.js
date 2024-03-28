a4f890fbebea99cb9ea23646c085fefe479ebf76
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-28 23:01:37 +0100

    Modification : Correction de la fonction qui permet d'afficher les utilisateurs , ajoute l'id lié au titre uniquement dans la colonne nécessaire au lieu de les ajouter dans la ligne et colonne à chaque itération

--- Jour04/job04/script.js
+++ Jour04/job04/script.js
@@ -12,21 +12,40 @@ async function fetchUtilisateurs () {
     .then(r => r.json()) // permet de parsé les éléments json
     .then(data => {
         button.addEventListener("click", () => {
+            tbody.innerHTML = "";
+            
             for (let i = 0; i < data.length ; i++) {
-                let ligne = document.createElement("tr");
-                for (let j = 0; j < data.length ; j++) {
-                    let colonne = document.createElement("td");
-                    let text = document.createTextNode(
-                    data[i]["prenom"]+ " " + data[j]["nom"],
-                );
-                colonne.appendChild(text)
-                ligne.appendChild(colonne)
-                }
-    
+                // Création de la ligne : va créer une ligne à chaque itération de la boucle par rapport à la taille du tableau
+                let ligne = document.createElement("tr"); 
+
+                // Création de la colonne id
+                let colonneId = document.createElement("td");
+                let textId = document.createTextNode(data[i]["id"]);
+                colonneId.appendChild(textId);
+                ligne.appendChild(colonneId);
+                
+                // Création de la colonne nom
+                let colonneNom = document.createElement("td");
+                let textNom = document.createTextNode(data[i]["nom"]);
+                colonneNom.appendChild(textNom);
+                ligne.appendChild(colonneNom);
+                
+                // Création de la colonne prenom
+                let colonnePrenom = document.createElement("td");
+                let textPrenom = document.createTextNode(data[i]["prenom"]);
+                colonnePrenom.appendChild(textPrenom);
+                ligne.appendChild(colonnePrenom);
+
+                // Création de la colonne email
+                let colonneEmail = document.createElement("td");
+                let textEmail = document.createTextNode(data[i]["email"]);
+                colonneEmail.appendChild(textEmail);
+                ligne.appendChild(colonneEmail);
+
                 tbody.appendChild(ligne)
             }
         })
-        
+
         })
     tableau.appendChild(tbody)
 }
