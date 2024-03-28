7c53572ee80ee1912e3cdd5cae58b5f157e3741d
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-28 22:37:12 +0100

    Ajout de fonction : Ajout de la fonctionnalité qui permet d'afficher les utilisateurs en appuyant sur le bouton afficher , difficulté pour le moment de bien placé les élément

--- Jour04/job04/script.js
+++ Jour04/job04/script.js
@@ -1,6 +1,34 @@
+// Récupération des élément html par son ID
+let button = document.getElementById("button")
+let tableau = document.getElementById("tableau")
+
+let tbody = document.createElement("tbody")
 
 
+// Création de la function d'appel des utilisateur JS
+async function fetchUtilisateurs () {
+    // appel du fichier json
+    const url = await fetch("utilisateur.json")
+    .then(r => r.json()) // permet de parsé les éléments json
+    .then(data => {
+        button.addEventListener("click", () => {
+            for (let i = 0; i < data.length ; i++) {
+                let ligne = document.createElement("tr");
+                for (let j = 0; j < data.length ; j++) {
+                    let colonne = document.createElement("td");
+                    let text = document.createTextNode(
+                    data[i]["prenom"]+ " " + data[j]["nom"],
+                );
+                colonne.appendChild(text)
+                ligne.appendChild(colonne)
+                }
+    
+                tbody.appendChild(ligne)
+            }
+        })
+        
+        })
+    tableau.appendChild(tbody)
+}
 
-
-// Création d'élément HTML pour pouvoir les rajouter par la suite sur le fichier
-
+fetchUtilisateurs()
