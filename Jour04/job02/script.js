875804f8d64862d764a6b237baab8e30d173dda6
YoussefGhollamallah <youssef.gh@live.fr>
2024-03-28 11:28:55 +0100

    Création d'un fichier javascript : Mise en place d'une fonction js avec une méthode fetch pour récupéré la ville d'un fichier json

---
+++ Jour04/job02/script.js
@@ -0,0 +1,15 @@
+let para = document.getElementById("ville");
+let button = document.getElementById("button");
+
+
+async function jsonValueKey() {
+   
+    button.addEventListener("click", () => {
+      const url = "plateforme.json"
+      fetch(url)
+      .then(response => response.json())
+      .then(data => para.innerHTML = data["city"])
+    })
+}
+
+jsonValueKey()
